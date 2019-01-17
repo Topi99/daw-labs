53302eeb6bddb20bd3d049c1675506544067fba2
Topiltzin <nokoayzack@gmail.com>
2019-01-16 20:13:10 -0600

    init structure

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import BrowserRouter from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+const App = () => {
+  return(
+    <main>
+      <nav>
+        nav
+      </nav>
+      <BrowserRouter>
+
+      </BrowserRouter>
+    </main>
+  );
+}
+
+export default App;
